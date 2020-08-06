0c5b52cdeb21c676dbc96218b4612acb0d070715
sonyavera <sonyavgould@gmail.com>
2020-08-06 15:21:35 -0400

    First commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  const toyList = document.getElementById("toy-collection")
+
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -11,5 +14,53 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       toyFormContainer.style.display = "none";
     }
+
+    function renderToy(toyObj){
+      const toyDisplayCard = document.createElement('div')
+      toyDisplayCard.classList.add('card')
+      toyDisplayCard.innerHTML =
+      `
+      <h2>${toyObj.name} </h2>
+      <img src="${toyObj.image}" class="image"/>
+      <p>Likes: ${toyObj.likes}</p>
+      <button class="like-btn" id="${toyObj.id}">Like <3</button>
+      `
+      toyList.append(toyDisplayCard)
+    }
+
+    function renderToys(toysCollection){
+      toysCollection.forEach(function(toyObj){
+        renderToy(toyObj)
+      })
+    }
+
+
+      fetch('http://localhost:3000/toys')
+      .then(response => response.json())
+      .then(toysCollection => renderToys(toysCollection))
+
+
+    const toyForm = e => {
+      
+    }
+
+  
+      let createToy = toy => {
+        return fetch("http://localhost:3000/toys", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+          },
+          body: JSON.stringify(toy)
+        })
+        .then(response => response.json())
+        .then(renderToy(toy))
+      }
+
+
+
+    
   });
+
 });
